de33d6339ea48f4a70d747e3865b2bcf450671af
Radostin Aristotelov <53051515+raristotelov@users.noreply.github.com>
2023-04-11 21:21:45 +0300

    Added firebase storage

---
+++ client/src/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+	apiKey: "AIzaSyCBnJFFVzyt-WcQnpmr6gFQ2RBHdfXDCmE",
+	authDomain: "picstagram-857d3.firebaseapp.com",
+	projectId: "picstagram-857d3",
+	storageBucket: "picstagram-857d3.appspot.com",
+	messagingSenderId: "684296567400",
+	appId: "1:684296567400:web:edd0dd40d5e31f683799dd"
+  };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+// Initialize Cloud Storage and get a reference to the service
+export const storage = getStorage(app);
