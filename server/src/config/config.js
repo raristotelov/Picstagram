a6b844d95bed6fd8c685aac9217272716a0bc9d1
Radostin Aristotelov <radostin.aristotelov@gmail.com>
2026-02-14 16:49:53 +0200

    Updated mongodb config file

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -1,14 +1,14 @@
-const env = process.env.NODE_ENV || "development";
+const env = process.env.NODE_ENV || 'development';
 
 const config = {
-  development: {
-    port: process.env.PORT || 5001,
-    dbUrl: "mongodb://localhost:27017/pictsagram",
-  },
-  production: {
-    port: process.env.PORT || 5001,
-    dbUrl: "mongodb://localhost:27017/pictsagram",
-  },
+	development: {
+		port: process.env.PORT || 5000,
+		dbUrl: process.env.MONGODB_URI,
+	},
+	production: {
+		port: process.env.PORT || 5000,
+		dbUrl: process.env.MONGODB_URI,
+	},
 };
 
 module.exports = config[env];
